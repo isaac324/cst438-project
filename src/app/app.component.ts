8a7104161d6807565a0c5506abe7f7fe454a7e02
Jeremy Shaw <jjsjeremy61@>
2019-12-06 21:20:49 -0800

    Frontend and backend coming together

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { Post } from './post';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
 
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/retry';
+import 'rxjs/add/observable/of';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
+
+  posts: Observable<any>;
+  newPost: Observable<any>;
+
+  constructor(private http: HttpClient) {}
+
+  getPosts() {
+    let params = new HttpParams().set('userId', '1');
+    let headers = new HttpHeaders().set('Authorization', 'auth-token');
+
+    this.posts = this.http.get(this.ROOT_URL + '/posts', { params, headers })    
+  }
+
+
+  createPost() {
+    const data: Post = {
+      id: null,
+      userId: 23,
+      title: 'My New Post',
+      body: 'Hello World!'
+    } 
+
+    this.newPost = this.http.post(this.ROOT_URL + '/posts', data)
+      // .retry(3)
+      // .catch(err => {
+      //   console.log(err)
+      //   return Observable.of(err)
+      // })
+
+
+  }
+
   title = 'cst438-project';
 }
