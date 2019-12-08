400286b6c44bc837af9fe0b4e81fcdc113de8516
Isaac <isaavila@csumb.edu>
2019-12-08 15:30:28 -0800

    update 12/8

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,7 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/retry';
 import 'rxjs/add/observable/of';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
